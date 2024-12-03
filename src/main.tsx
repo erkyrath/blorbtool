b5a374186932425c25e76d0da681247bb248cf09
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-02 19:28:30 -0500

    Renders.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,20 @@
-import { createRoot } from 'react-dom/client';
+import { Root, createRoot } from 'react-dom/client';
+
+let root: Root|null = null;
 
 export function init() {
     document.body.innerHTML = '<div id="app"></div>';
     const appel = document.getElementById('app') as HTMLElement;
-    const root = createRoot(appel);
+    root = createRoot(appel);
+    render();
+}
+
+export function render() {
     if (root)
         root.render(<h1>Hello, world</h1>);
+}
+
+export function render2() {
+    if (root)
+        root.render(<h1>Goodbye, world</h1>);
 }
