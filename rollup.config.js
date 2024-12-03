fb5356d8abab4d55aaba3a3b31c75d0d42a06a41
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-02 19:17:21 -0500

    Rejigger for React and tsx files.

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,23 @@
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
 
 export default {
-    input: 'lib/test.js',
+    input: 'lib/main.js',
     output: {
         file: 'js/bundle.js',
         name: 'bundle',
         format: 'iife'
     },
     plugins: [
-        terser()
+        replace({
+            'preventAssignment': true,
+            'process.env.NODE_ENV': JSON.stringify('production')
+        }),
+        commonjs(),
+        nodeResolve(),
+        //terser()
     ]
 }
 
