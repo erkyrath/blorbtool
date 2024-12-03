433518d1b8076e9adb5415b11df861159f0b5775
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-03 14:03:19 -0500

    Example checkpoint.

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,65 @@
+import React from 'react';
+import { useState } from 'react';
 import { Root, createRoot } from 'react-dom/client';
 
 let root: Root|null = null;
 
-export function init() {
+export function init()
+{
     document.body.innerHTML = '<div id="app"></div>';
     const appel = document.getElementById('app') as HTMLElement;
     root = createRoot(appel);
-    render();
+    if (root)
+        root.render( <MyApp /> );
 }
 
-export function render() {
-    if (root)
-        root.render(<h1>Hello, world</h1>);
+function MyApp()
+{
+    const [count, setCount] = useState(1);
+    
+    function handleButtonClick()
+    {
+        console.log('### click');
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton count={ count } onClick={ handleButtonClick } />
+            <MyList />
+        </div>
+    );
 }
 
-export function render2() {
-    if (root)
-        root.render(<h1>Goodbye, world</h1>);
+type MouseEv = React.MouseEventHandler<HTMLButtonElement>;
+
+function MyButton({ count, onClick } : { count:number, onClick:MouseEv } )
+{
+    return (
+        <button className="Button" onClick={ onClick }>
+            Counter: { count }
+        </button>
+    );
+}
+
+const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+];
+
+function MyList()
+{
+    const ells = products.map(product =>
+        <li key={ product.id }>
+            <em>{ product.id }</em>: { product.title }
+        </li>
+    );
+    
+    return (
+        <ul>
+            { ells }
+        </ul>
+    );
 }
