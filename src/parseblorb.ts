a1469f0286dec2062fc91afcdf8eab9e24ad066e
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-04 15:01:42 -0500

    Remove console log lines.

--- src/parseblorb.ts
+++ src/parseblorb.ts
@@ -52,12 +52,10 @@ export function parse_blorb(dat: Uint8Array, filename?: string) : Blorb
         if (ctype == 'FORM') {
             let formtype = u8ToString(dat, pos, 4);
             let cdat = dat.slice(cpos, pos+clen);
-            console.log('###', ctype, formtype, clen+'+8', cpos);
             chunk = new_chunk(uctype, cdat);
         }
         else {
             let cdat = dat.slice(pos, pos+clen);
-            console.log('###', ctype, clen, cpos);
             chunk = new_chunk(uctype, cdat);
         }
 
