ff694af200722958ba3a0c179287dc88836e8b8c
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-04 14:10:57 -0500

    Start the index col.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState, useReducer } from 'react';
 import { Root, createRoot } from 'react-dom/client';
 
-import { Blorb, new_blorb } from './parseblorb';
+import { Chunk, Blorb, new_blorb } from './parseblorb';
 import { parse_blorb } from './parseblorb';
 
 let initialBlorb: Blorb|undefined;
@@ -27,6 +27,10 @@ function MyApp()
     
     const [blorb, dispBlorb] = useReducer(reduceBlorb, initialBlorb!);
 
+    let chunkls = blorb.chunks.map(chunk =>
+        <li>CHUNK</li>
+    );
+    
     return (
         <>
             <div className="IndexCol">
@@ -35,6 +39,9 @@ function MyApp()
                     <div className="BlorbGloss">
                     { blorb.chunks.length } chunks, { blorb.totallen } bytes</div>
                 </div>
+                <ul className="ChunkList">
+                    { chunkls }
+                </ul>
             </div>
             <div className="DisplayCol">
             </div>
@@ -42,6 +49,13 @@ function MyApp()
     );
 }
 
+function ChunkListEntry({ chunk, blorb } : { chunk:Chunk, blorb:Blorb } )
+{
+    return (
+        <li>
+        </li>
+    );
+}
 
 function reduceBlorb(blorb: Blorb, act: any) : Blorb
 {
