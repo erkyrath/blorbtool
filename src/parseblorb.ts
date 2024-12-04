dceb72bff9bb13951ebd6550eadbf3610e6a469f
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-03 19:29:21 -0500

    The chunk read loop.

--- src/parseblorb.ts
+++ src/parseblorb.ts
@@ -12,9 +12,14 @@ function u8ToString(arr: Uint8Array,
         if (len === undefined)
             arr = arr.slice(rpos);
         else
-            arr = arr.slice(rpos, len);
+            arr = arr.slice(rpos, rpos+len);
     }
     return [ ...arr ].map(ch => String.fromCharCode(ch) ).join('');
+}
+
+function u8read4(arr: Uint8Array, pos: number) : number
+{
+    return arr[pos] * 0x1000000 + arr[pos+1] * 0x10000 + arr[pos+2] * 0x100 + arr[pos+3];
 }
 
 type Chunk = {
@@ -36,7 +41,8 @@ export function new_blorb() : Blorb
 export function parse_blorb(dat: Uint8Array) : Blorb
 {
     let pos = 0;
-    let len = dat.length;
+    const len = dat.length;
+    
     if (len < 12) {
         console.log('### Too short to be a valid blorb file');
         return new_blorb();
@@ -45,6 +51,42 @@ export function parse_blorb(dat: Uint8Array) : Blorb
     if (u8ToString(dat, 0, 4) != 'FORM') {
         console.log('### This does not appear to be a valid blorb file');
         return new_blorb();
+    }
+
+    let foundlen = u8read4(dat, 4);
+    if (foundlen+8 != len) {
+        console.log('### Blorb length field is incorrect');
+        // continue
+    }
+
+    if (u8ToString(dat, 8, 4) != 'IFRS') {
+        console.log('### This does not appear to be a valid blorb file');
+        return new_blorb();
+    }
+
+    let chunks = [];
+    pos = 12;
+
+    while (pos < len) {
+        if (pos+8 > len) {
+            console.log('### Blorb data looks truncated');
+            break;
+        }
+        let ctype = u8ToString(dat, pos, 4);
+        let clen = u8read4(dat, pos+4);
+        pos += 8;
+
+        if (pos+clen > len) {
+            console.log('### Blorb chunk looks truncated');
+            break;
+        }
+
+        let cdat = dat.slice(pos, pos+clen);
+        console.log('###', ctype, clen, pos-8);
+
+        pos += clen;
+        if (pos & 1)
+            pos++;
     }
 
     let blorb = { chunks: [] };
