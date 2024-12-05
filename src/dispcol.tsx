1eb76178fb5c1bea50f81325e663b9e7d6d41cf9
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-05 11:35:08 -0500

    Split that out.

--- src/dispcol.tsx
+++ src/dispcol.tsx
@@ -8,11 +8,7 @@ export function DisplayChunk({ blorb, chunk } : { blorb:Blorb, chunk:Chunk })
 {
     let resentry = blorb_resentry_for_chunk(blorb, chunk);
 
-    let subdata = chunk.data.slice(0, 512);
-    let ls = [ ...subdata ].map(byte_to_hex);
-    let hexdump = ls.join(' ');
-
-    let extra = chunk.data.length - subdata.length;
+    let display = DisplayChunkRaw(blorb, chunk);
     
     return (
         <div className="DisplayChunk">
@@ -38,15 +34,28 @@ export function DisplayChunk({ blorb, chunk } : { blorb:Blorb, chunk:Chunk })
                     { pretty_size(chunk.data.length) }
                 </li>
                 <li>
-                    <span className="InfoLabel">File position:</span> { chunk.pos }</li>
+                    <span className="InfoLabel">File position:</span> { chunk.pos }
+                </li>
             </ul>
+            { display }
+        </div>
+    );
+}
+
+export function DisplayChunkRaw(blorb: Blorb, chunk: Chunk)
+{
+    let subdata = chunk.data.slice(0, 512);
+    let ls = [ ...subdata ].map(byte_to_hex);
+    let hexdump = ls.join(' ');
+
+    let extra = chunk.data.length - subdata.length;
+
+    return (
             <div>
                 <code className="HexData">{ hexdump }</code>
                 { ( extra ?
                     <span className="InfoLabel"> (...{ extra } more)</span>
                     : null) }
             </div>
-        </div>
     );
 }
-
