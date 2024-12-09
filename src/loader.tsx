4fb24199e3d2c663e6457d7a7e2d016e4825c335
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-09 12:18:33 -0500

    Loader.

--- src/loader.tsx
+++ src/loader.tsx
@@ -4,20 +4,30 @@ import { AboutPane } from './about';
 
 export function LoaderIndex()
 {
+    let filetypes = '.blb,.blorb,.zblorb,.gblorb,.ablorb,application-xblorb';
+
+    function evhan_change(ev: ChangeEv) {
+        console.log('### got', ev);
+    };
+    
     return (
-	<div>Welcome.</div>
+        <div>
+            <input type="file" accept={ filetypes } onChange= { evhan_change } />
+        </div>
     );
 }
 
 export function LoaderDisplay()
 {
     return (
-	<>
+        <>
             <div className="DisplayHeader">
-	    </div>
+            </div>
             <div className="DisplayPane">
-		<AboutPane />
-	    </div>
-	</>
+                <AboutPane />
+            </div>
+        </>
     );
 }
+
+type ChangeEv = React.ChangeEvent<HTMLInputElement>;
