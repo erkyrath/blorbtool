eeb3a408e94dc7f7e208bdea5658e07af763b132
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-09 12:54:43 -0500

    Need more contexts.

--- src/contexts.ts
+++ src/contexts.ts
@@ -1,10 +1,11 @@
 import { createContext } from 'react';
 
-import { new_blorb } from './blorb';
+import { Blorb, new_blorb } from './blorb';
 
 export type AltDisplay = 'about' | 'errors' | null;
 export type ChunkCmd = 'download' | 'delete' | null;
 export type BlorbCmd = 'download' | 'addchunk' | null;
+export type LoadBlorbAction = { blorb:Blorb, filename:string, data:Uint8Array };
 
 export const SelectionCtx = createContext(-1);
 export const SetSelectionCtx = createContext((val:number) => {});
@@ -19,5 +20,7 @@ export const BlorbCmdCtx = createContext(null as BlorbCmd);
 export const SetBlorbCmdCtx = createContext((val:BlorbCmd) => {});
 
 export const BlorbCtx = createContext(new_blorb());
+export const LoadBlorbCtx = createContext((act:LoadBlorbAction) => {});
+
 
 
