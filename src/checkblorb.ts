1e6325f90d9012af739c724bd15168f9e6665188
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-12 15:20:38 -0500

    Here's our model for error checking.

--- src/checkblorb.ts
+++ src/checkblorb.ts
@@ -1,5 +1,6 @@
-import { Chunk } from './chunk';
+import { Chunk, CTypes } from './chunk';
 import { Blorb } from './blorb';
+import { blorb_chunk_for_usage } from './blorb';
 
 export function check_blorb_against_origpos(blorb: Blorb, origlen: number, map: Map<number, number>) : Blorb
 {
@@ -29,6 +30,14 @@ export function check_blorb_consistency(blorb: Blorb) : Blorb
 {
     let errors: string[] = []; // new set of errors
 
+    for (let chunk of blorb.chunks) {
+        switch (chunk.type.stype) {
+        case 'Fspc':
+            check_chunk_Fspc(blorb, chunk as CTypes.CTFrontispiece, errors);
+            break;
+        }
+    }
+
     if (errors.length) {
         blorb = {
             ...blorb,
@@ -37,4 +46,12 @@ export function check_blorb_consistency(blorb: Blorb) : Blorb
     }
 
     return blorb;
+}
+
+function check_chunk_Fspc(blorb: Blorb, chunk: CTypes.CTFrontispiece, errors: string[])
+{
+    let imgchunk = blorb_chunk_for_usage(blorb, 'Pict', chunk.picnum);
+    if (!imgchunk) {
+        errors.push(`Frontispiece chunk refers to Pict #${chunk.picnum}, but there is no such resource.`);
+    }
 }
