a075c9938e4c4586e68f546d541c243d27fc0c23
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-18 11:10:31 -0500

    Comments.

--- src/blorb.ts
+++ src/blorb.ts
@@ -2,8 +2,32 @@ import { u8ToString, stringToU8 } from './datutil';
 import { u8read4, u8write4 } from './datutil';
 import { Chunk, CTypes } from './chunk';
 
+/* The Blorb type and functions that manipulate it (at a low level).
+
+   A Blorb object is fundamentally a list of chunks, but we keep a bunch
+   of lookup tables for fast access. Also a list of errors to display,
+   if the current state of the Blorb has errors. The lookup tables
+   and errors are rebuilt every time we update the Blorb.
+   
+   I say "update", but in fact the Blorb object (and all chunk objects)
+   are considered *immutable*. We *never* update anything in place. (Not
+   even the lookup tables!) The plan is always to create a new object
+   with updated contents.
+
+   Happily, JS's spread operator makes this fast and cheap and even easy
+   to read. (Since everything is immutable, shallow copies are always
+   safe.)
+
+   The immutable plan allows undo/redo to work in a simple way. Of course
+   I haven't implemented undo/redo yet. But it's the thought that counts.
+ */
+
+/* Slight hack: an error can be a text message *or* a text plus a reference
+   to the chunk that caused it. I built a lot of string arrays before
+   I thought of this, so I went with this "|" type. */
 export type Error = { text:string, refkey:number} | string;
 
+/* The mighty Blorb. */
 export type Blorb = {
     filename: string|undefined;
     chunks: ReadonlyArray<Chunk>;
@@ -19,6 +43,11 @@ export type Blorb = {
     errors: ReadonlyArray<Error>;
 };
 
+/* Return a completely empty Blorb.
+   Note that a valid Blorb must contain a resource index chunk (RIdx)
+   as its first chunk. This doesn't have that. But we're using this as
+   a base to build on, so it's okay as a temporary thing.
+*/
 export function new_blorb() : Blorb
 {
     return {
@@ -33,6 +62,11 @@ export function new_blorb() : Blorb
     };
 }
 
+/* Get the byte-array form of a Blorb.
+   This is relatively simple because every Chunk keeps *its* byte-array
+   form up to date. (Or it should!) So this is mostly a matter of
+   concatenating them all.
+*/
 export function blorb_get_data(blorb: Blorb) : Uint8Array
 {
     let data = new Uint8Array(blorb.totallen);
@@ -67,6 +101,21 @@ export function blorb_get_data(blorb: Blorb) : Uint8Array
     return data;
 }
 
+/* Recompute the position field of every chunk, and also the position data
+   in the index chunk. Also the fast lookup tables.
+   
+   This is a low-level function; we call this after deleting or modifying
+   any chunk. The oldusagemap associates resids ("Pict:1") with chunk
+   refkeys. We need this to rebuild the index chunk, whose entries refer
+   to chunks by position and are therefore all out of date. (The positions
+   all just changed!)
+
+   We also call it after loading a brand-new Blorb from a byte array.
+   In that case the position fields and index chunk should already be
+   correct (we hope!) so it's just a matter of creating the lookup tables.
+   (In this case oldusagemap is not provided, because there is no
+   "before loading" and no positions have changed.)
+*/
 export function blorb_recompute_positions(blorb: Blorb, oldusagemap?: Map<string, number>) : Blorb
 {
     if (blorb.chunks.length == 0)
@@ -208,11 +257,18 @@ export function blorb_recompute_positions(blorb: Blorb, oldusagemap?: Map<string
     };
 }
 
+/* Wipe the error list for a Blorb. (We do this before any modification,
+   and then create a new error list afterwards.)
+*/
 export function blorb_clear_errors(blorb: Blorb) : Blorb
 {
     return { ...blorb, errors: [] };
 }
 
+/* Update the index chunk with a new list of entries. (Usually this is
+   the old list with one entry deleted or modified. But it could be
+   more.)
+*/
 export function blorb_update_index_entries(blorb: Blorb, entries: CTypes.CTResIndexEntry[]) : Blorb
 {
     if (blorb.chunks.length == 0)
@@ -243,6 +299,14 @@ export function blorb_update_index_entries(blorb: Blorb, entries: CTypes.CTResIn
     return newblorb;
 }
 
+/* Delete one chunk.
+   This is a low-level function; it ensures that the index chunk is
+   updated, but it doesn't consider other chunks that might be
+   affected. (The resource description chunk, the frontispiece chunk,
+   etc -- anything that might refer *to* the deleted chunk.) See
+   the function in editblorb.ts for the high-level code that deals
+   with this.
+ */
 export function blorb_delete_chunk_by_key(blorb: Blorb, key: number) : Blorb
 {
     let chunk = blorb.keymap.get(key);
@@ -258,17 +322,25 @@ export function blorb_delete_chunk_by_key(blorb: Blorb, key: number) : Blorb
     let newchunks = blorb.chunks.filter((chu) => (chu.refkey != key));
     let newblorb: Blorb = { ...blorb, chunks:newchunks };
 
+    /* blorb_recompute_positions() will take care of deleting the
+       index chunk reference. */
     newblorb = blorb_recompute_positions(newblorb, blorb.usagemap);
     
     return newblorb;
 }
 
+/* Replace a chunk with another of the same type.
+   
+   This is only meant for singleton chunk types! Don't call it on an
+   image chunk, for example. (There could be multiple JPEG chunks in
+   the file, and this just replaces the first one. Not what you want.)
+
+   Note that the chunk argument's refkey is updated to match the
+   chunk it's replacing.
+ */
 export function blorb_addreplace_chunk(blorb: Blorb, chunk: Chunk) : Blorb
 {
-    /* Only for singleton chunk types! Which are, by definition, not
-       resources.
-       (We keep the refkey when replacing a chunk, mind you.)
-    */
+
     let pos = blorb.chunks.findIndex((chu) => (chu.type.stype == chunk.type.stype));
 
     let newblorb: Blorb;
@@ -289,16 +361,23 @@ export function blorb_addreplace_chunk(blorb: Blorb, chunk: Chunk) : Blorb
     return newblorb;
 }
 
+/* And now, accessors. */
+
+/* First chunk of a given type. This should only be used for singleton
+   chunk types.
+*/
 export function blorb_first_chunk_for_type(blorb: Blorb, type: string) : Chunk|undefined
 {
     return blorb.typemap.get(type);
 }
 
+/* The chunk with a given refkey. */
 export function blorb_chunk_for_key(blorb: Blorb, key: number) : Chunk|undefined
 {
     return blorb.keymap.get(key);
 }
 
+/* The index chunk entry for a given chunk. (If it is a resource.) */
 export function blorb_resentry_for_chunk(blorb: Blorb, chunk: Chunk) : CTypes.CTResIndexEntry|undefined
 {
     if (blorb.chunks.length == 0 || blorb.chunks[0].type.stype != 'RIdx')
@@ -308,6 +387,7 @@ export function blorb_resentry_for_chunk(blorb: Blorb, chunk: Chunk) : CTypes.CT
     return ridx.invusagemap.get(chunk.pos);
 }
 
+/* The index chunk entry for a given chunk (by refkey). */
 export function blorb_resentry_for_key(blorb: Blorb, key: number) : CTypes.CTResIndexEntry|undefined
 {
     let chunk = blorb.keymap.get(key);
@@ -316,6 +396,7 @@ export function blorb_resentry_for_key(blorb: Blorb, key: number) : CTypes.CTRes
     return blorb_resentry_for_chunk(blorb, chunk);
 }
 
+/* The chunk whose index entry matches a given resource usage/number. */
 export function blorb_chunk_for_usage(blorb: Blorb, usage: string, resnum: number) : Chunk|undefined
 {
     if (blorb.chunks.length == 0 || blorb.chunks[0].type.stype != 'RIdx')
