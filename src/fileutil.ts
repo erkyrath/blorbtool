4f08709130dc75da11ae11cecf49765c76805a8a
Andrew Plotkin <zarf@ZarfLent.local>
2024-12-19 15:20:14 -0500

    Comments.

--- src/fileutil.ts
+++ src/fileutil.ts
@@ -1,6 +1,24 @@
 import { u8ToString, looksLikeAscii } from './datutil';
 import { find_dimensions_png, find_dimensions_jpeg } from './imgutil';
 
+/* In these functions, file types are mostly the same as Blorb
+   chunk types. That's just a handy scheme which we're already familiar
+   with.
+
+   There are some exceptions:
+
+   - AIFF files are 'AIFF', even though the chunk type is 'FORM' with
+       'AIFF' as the subtype.
+   - We refer to Blorb files as 'IFRS'. Again, the chunk type is 'FORM'
+       with a subtype, and we wouldn't expect a Blorb chunk anyhow.
+       But we should have a filetype name for it.
+   - We don't distinguish AUTH, ANNO, or other text file types.
+       They're all 'TEXT'.
+   - We don't distinguish the special Blorb binary chunks like RIdx
+       or RDes. They're just 'BINA' as files.
+*/
+
+/* Return a human-readable description of a file type. */
 export function filetype_readable_desc(filetype: string) : string
 {
     switch (filetype) {
@@ -48,6 +66,10 @@ export type FileTypeGuess = {
     alttype: string|undefined;
 };
 
+/* Given a file and its filename, return our best guess as to its type.
+   The return structure has a filetype and possibly an alttype, if
+   we have a second-best guess.
+*/
 export function determine_file_type(filename: string, data: Uint8Array) : FileTypeGuess
 {
     let fnguess: string|undefined;
@@ -84,14 +106,19 @@ export function determine_file_type(filename: string, data: Uint8Array) : FileTy
         datguess = 'ZCOD';
     }
 
-    console.log('### guesses:', fnguess, datguess);
-    
+    /* If the two guesses agree, great. If we only had one good guess,
+       go with that. */
+
     if (fnguess && datguess && fnguess == datguess) 
         return { filetype:fnguess, alttype:undefined };
     if (fnguess && !datguess) 
         return { filetype:fnguess, alttype:undefined };
     if (datguess && !fnguess) 
         return { filetype:datguess, alttype:undefined };
+
+    /* Otherwise, return both (if we had two) or 'BINA' (if we had
+       zero). */
+    
     if (!datguess && !fnguess)
         return { filetype:'BINA', alttype: undefined };
     return { filetype:(fnguess || 'BINA'), alttype:datguess };
